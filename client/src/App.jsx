a42d6c256ca5caad7aa06f6e516c4399b8c91026
yhunlu <yhunlu87@gmail.com>
2022-02-07 01:21:13 +0300

    initialize frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import {
+  Footer,
+  Loader,
+  Navbar,
+  Services,
+  Transactions,
+  Welcome,
+} from './components';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
