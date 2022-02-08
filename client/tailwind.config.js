748e5f86a50336c5f6c14c9c86c3fafac39d47f3
yhunlu <yhunlu87@gmail.com>
2022-02-09 01:34:07 +0300

    blockchain setup done

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  mode: 'jit',
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  media: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       display: ['Open Sans', 'sans-serif'],
