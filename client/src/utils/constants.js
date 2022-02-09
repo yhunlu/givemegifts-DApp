d96d6f24165d44d23a5707f64e08040ed0b1402b
yhunlu <yhunlu87@gmail.com>
2022-02-09 03:17:53 +0300

    smart-contracts working, start to connect wallet

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAdress = '0x6F5aD5D9eCA204439aC6D9f65d96ac27809A4a2b';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/h-u9tKA6yVpLOdhltW67Ae70FOfe7ti-',
+      accounts: [
+        'a6535a4fbd941934ae362b789ca65b0ab33740421563821b599c39c5078c1846',
+      ],
+    },
+  },
 };
