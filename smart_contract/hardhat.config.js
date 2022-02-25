5700513a580a7db02733c5407be16c57c6002a84
yhunlu <yhunlu87@gmail.com>
2022-02-25 03:09:05 +0300

    fixed privatekey issue.

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,14 @@
 require('@nomiclabs/hardhat-waffle');
 
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
+
 module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/h-u9tKA6yVpLOdhltW67Ae70FOfe7ti-',
-      accounts: [
-        'a6535a4fbd941934ae362b789ca65b0ab33740421563821b599c39c5078c1846',
-      ],
+      accounts: [privateKey],
     },
   },
 };
